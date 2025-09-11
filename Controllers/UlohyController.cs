ee13c445649ac2ffc921167524b536936a8e513b
sssamooo <155848229+sssamooo@users.noreply.github.com>
2025-09-11 08:15:06 +0200

    1. Uloha

---
+++ Controllers/UlohyController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace stejskal_4D_hello_aspnet.Controllers
+{
+    public class UlohyController : Controller
+    {
+        public IActionResult Uloha1()
+        {
+            return View();
+        }
+    }
+}
--- obj/Debug/net8.0/a.AssemblyInfo.cs
+++ obj/Debug/net8.0/a.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("a")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1896452890d6fc51f054503805c5ec5d0ac4cb28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0918a5b0739aae00bce31e695ed493c820114b49")]
 [assembly: System.Reflection.AssemblyProductAttribute("a")]
 [assembly: System.Reflection.AssemblyTitleAttribute("a")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
